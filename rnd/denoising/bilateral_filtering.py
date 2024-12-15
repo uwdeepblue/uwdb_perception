1febe6e2a188193f2ccfaf801dcc0161218b7f31
B3ngineering <98676125+B3ngineering@users.noreply.github.com>
2024-12-15 14:47:41 -0400

    Refactoring denoising scripts to function-based format for easy development and export.

--- rnd/denoising/bilateral_filtering.py
+++ rnd/denoising/bilateral_filtering.py
@@ -2,13 +2,16 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Load an image
-image = cv2.imread("images/noisy_butterfly.jpg", cv2.IMREAD_GRAYSCALE)  # Use IMREAD_COLOR for color images
+def bilateral_filtering(image):
+    # Apply Bilateral Filtering
+    bilateral_filtered = cv2.bilateralFilter(image, d=9, sigmaColor=75, sigmaSpace=75)
 
-# Apply Bilateral Filtering
-bilateral_filtered = cv2.bilateralFilter(image, d=9, sigmaColor=75, sigmaSpace=75)
+    # Display the result
+    plt.subplot(1, 2, 1), plt.imshow(image, cmap='gray'), plt.title('Original')
+    plt.subplot(1, 2, 2), plt.imshow(bilateral_filtered, cmap='gray'), plt.title('Bilateral Filter')
+    plt.show()
 
-# Display the result
-plt.subplot(1, 2, 1), plt.imshow(image, cmap='gray'), plt.title('Original')
-plt.subplot(1, 2, 2), plt.imshow(bilateral_filtered, cmap='gray'), plt.title('Bilateral Filter')
-plt.show()
+if __name__ == "__main__":
+    # Load an image
+    image = cv2.imread("images/noisy_butterfly.jpg", cv2.IMREAD_GRAYSCALE)  # Use IMREAD_COLOR for color images
+    bilateral_filtering(image)
--- rnd/denoising/nonlocal_means.py
+++ rnd/denoising/nonlocal_means.py
@@ -2,13 +2,17 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Load an image
-image = cv2.imread("images/noisy_butterfly.jpg", cv2.IMREAD_GRAYSCALE)  # Use IMREAD_COLOR for color images
+def nonlocal_means_denoising(image):
+    # Apply Non-Local Means Denoising
+    denoised_image = cv2.fastNlMeansDenoising(image, None, h=10, templateWindowSize=7, searchWindowSize=21)
 
-# Apply Non-Local Means Denoising
-denoised_image = cv2.fastNlMeansDenoising(image, None, h=10, templateWindowSize=7, searchWindowSize=21)
+    # Display the result
+    plt.subplot(1, 2, 1), plt.imshow(image, cmap='gray'), plt.title('Original')
+    plt.subplot(1, 2, 2), plt.imshow(denoised_image, cmap='gray'), plt.title('Denoised')
+    plt.show()
 
-# Display the result
-plt.subplot(1, 2, 1), plt.imshow(image, cmap='gray'), plt.title('Original')
-plt.subplot(1, 2, 2), plt.imshow(denoised_image, cmap='gray'), plt.title('Denoised')
-plt.show()
+
+if __name__ == "__main__":
+    # Load image
+    image = cv2.imread("images/noisy_butterfly.jpg", cv2.IMREAD_GRAYSCALE)  # Use IMREAD_COLOR for color images
+    nonlocal_means_denoising(image)
