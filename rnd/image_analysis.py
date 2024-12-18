95b3bd46eaba72112fc2b05d79e2029d1d5f9a81
B3ngineering <98676125+B3ngineering@users.noreply.github.com>
2024-12-18 15:55:11 -0400

    Structuring tool with imports and user input

--- rnd/image_analysis.py
+++ rnd/image_analysis.py
@@ -1,10 +1,14 @@
 import cv2
-
+import numpy as np
+from matplotlib import pyplot as plt
 from denoising.denoising_techniques.py import nonlocal_means_denoising, bilateral_filtering, wavelet_transform
 
 if __name__ == "__main__":
-    # Load image
-    image = cv2.imread("images/noisy_butterfly.jpg", cv2.IMREAD_GRAYSCALE)  # Use IMREAD_COLOR for color images
+    # Take user input for which image to use
+    image_path = input("Enter image path: ")
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # Use IMREAD_COLOR for color images
+
+    # Take user input for which denoising technique to use
     print("Select denoising technique: ")
     print("1. Non-Local Means Denoising")
     print("2. Bilateral Filtering")
@@ -19,3 +23,9 @@ if __name__ == "__main__":
         wavelet_transform(image)
     else:
         print("Invalid choice")
+    
+    # Load in the new version of the image
+
+    # Take user input for which openCV classifier to use
+
+    # Save final image and results for later comparison
